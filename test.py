972465c1a65a71d35b7cc458b47e0bf8b178d47c
stilletto <avatarnarod@gmail.com>
2024-06-27 10:30:21 +0400

    update

--- test.py
+++ test.py
@@ -21,6 +21,8 @@ from transformers import DataCollatorForLanguageModeling
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import rdMolDescriptors
+from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+
 
 # Имя файла-сигнала
 signal_file = 'stop_signal.txt'
@@ -41,10 +43,30 @@ def monitor_keyboard_signal(signal_file):
 # Основной код обучения
 # Загрузка датасета
 dataset_name = "stilletto/target_affinity"
+
+
+
+custom_loss = True
+lora = True
+#model_name = "models/mistral"
+base_model = "models/mistral-inst"
+inst_model = True
+#base_model = "models/Mistral-7B-finetuned"
+model_name = base_model
+model_name = "results/checkpoint-42600"
+device = "cuda"
+
+separator0 = "[INST]"
+separator1 = "[/INST]"
+if inst_model:
+    separator0 = "Create"
+    separator1 = "format."
+
+
 def filter_dataset(dataset_name, cache_dir=None):
     dataset = load_dataset(dataset_name, split="train", cache_dir=cache_dir)
     def filter_func(example):
-        if '[/INST]' in example['text']:
+        if separator1 in example['text']:
             index = example['text'].index('[/INST]')
             return index <= 800
         return False
@@ -54,9 +76,7 @@ def filter_dataset(dataset_name, cache_dir=None):
 dataset = filter_dataset(dataset_name)
 print(f"dataset len={len(dataset)}")
 
-model_name = "models/mistral"
-base_model = "models/mistral"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(base_model)
 tokenizer.padding_side = 'right'
 tokenizer.pad_token = tokenizer.eos_token
 
@@ -103,6 +123,8 @@ def find_amino_acids(mol):
     for aa in amino_acids_mols:
         if mol.HasSubstructMatch(aa):
             found_amino_acids.append(aa)
+    if len(found_amino_acids)>0:
+        print("found amino acids!")
     return found_amino_acids
 
 def has_peptide_bonds(mol):
@@ -112,6 +134,7 @@ def has_peptide_bonds(mol):
     for smarts in peptide_bond_smarts:
         bond_mol = Chem.MolFromSmarts(smarts)
         if mol.HasSubstructMatch(bond_mol):
+            print("HAS PEPTIDE BONDS!")
             return True
     return False
 
@@ -144,19 +167,36 @@ def is_peptide(smiles: str) -> bool:
     return has_peptide_bonds_flag and has_n_term and has_c_term
 
 def loss_for_smiles(output):
-    if "/INST]" not in output:
-        return 10
+    if not inst_model:
+        if "/INST]" not in output:
+            print("not even /INST]")
+            return 10
+        else:
+            original_output = output
+            output = output.split("/INST]")[1]
+            if "</s>" in output:
+                output = output.split("</s>")[0]
+            if not is_valid_smiles(output):
+                return 5
+            if "peptide" in original_output.lower():
+                if is_peptide(output):
+                    return 0.1
+            return 1
     else:
-        original_output = output
-        output = output.split("/INST]")[1]
-        if "</s>" in output:
-            output = output.split("</s>")[0]
-        if not is_valid_smiles(output):
-            return 5
-        if "peptide" in original_output.lower():
-            if is_peptide(output):
-                return 0.1
-        return 1
+        if "format." not in output:
+            print("not even format.")
+            return 10
+        else:
+            original_output = output
+            output = output.split("format.")[1]
+            if "</s>" in output:
+                output = output.split("</s>")[0]
+            if not is_valid_smiles(output):
+                return 5
+            if "peptide" in original_output.lower():
+                if is_peptide(output):
+                    return 0.1
+            return 1
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
@@ -169,19 +209,15 @@ class advanced_SFTTrainer(SFTTrainer):
         self.label_smoother = LabelSmoother()
     def compute_loss(self, model, inputs, return_outputs=False):
         input_ids = inputs.get('input_ids')
-        inst_model = False
-        separator0 = "[INST]"
-        separator1 = "[/INST]"
-        if inst_model:
-            separator1 = "format."
-        labels = inputs.pop('labels')
+
+        label = inputs.pop('labels')
         decoded_inputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-        decoded_labels = self.tokenizer.batch_decode(labels, skip_special_tokens=True)
+        decoded_labels = decoded_inputs
         decoded_inputs = [decoded_inputs[0].split(",")[0]]
-        print("Decoded inputs: ", decoded_inputs)
+        #print("Decoded inputs: ", decoded_inputs)
         pref = decoded_labels[0].split(",")[0]
-        print("Decoded labels: ", decoded_labels)
-        pref = pref.split(separator1)[0] + separator0
+        #print("Decoded labels: ", decoded_labels)
+        pref = pref.split(separator1)[0] + separator1
         after_inst = decoded_labels[0].split(separator1)[1]
         label_list = after_inst.split(",")
         labels = []
@@ -190,9 +226,14 @@ class advanced_SFTTrainer(SFTTrainer):
             a = pref + alt
             if len(a) > max_length_input:
                 max_length_input = len(a)
+        i = 0
         for alt in label_list:
             a = pref + alt
+
             combo_label = [a]
+            if i == 0:
+                decoded_inputs = combo_label
+            i += 1
             new_alt = self.tokenizer.batch_encode_plus(
                 combo_label, padding='max_length', max_length=max_length_input, truncation=False, return_tensors='pt'
             )['input_ids']
@@ -212,6 +253,7 @@ class advanced_SFTTrainer(SFTTrainer):
         print("Decoded inputs: ", decoded_inputs)
         print("Decoded outputs: ", decoded_outputs)
         loss_multiplier = loss_for_smiles(decoded_outputs[0])
+        print(f"Loss multiplies={loss_multiplier}")
         alternative_losses = []
         for alt_label in labels:
             alternative_loss = self.label_smoother(outputs, alt_label, shift_labels=True)
@@ -225,60 +267,162 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype=torch.bfloat16,
     bnb_4bit_use_double_quant=False,
 )
-model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config=bnb_config,
-        torch_dtype=torch.bfloat16,
-        device_map="auto",
-        trust_remote_code=True,
-)
-model.config.use_cache = False
-model.gradient_checkpointing_enable()
-model = prepare_model_for_kbit_training(model)
-peft_config = LoraConfig(
-    lora_alpha=16,
-    lora_dropout=0.1,
-    r=512,
-    bias="none",
-    task_type="CAUSAL_LM",
-    use_rslora=True,
-    use_dora=True,
-)
-model = get_peft_model(model, peft_config)
+if device == "cpu":
+    model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            device_map="cpu",
+            trust_remote_code=True,
+    )
+else:
+    if lora:
+        model = AutoModelForCausalLM.from_pretrained(
+                model_name,
+                quantization_config=bnb_config,
+                torch_dtype=torch.bfloat16,
+                device_map="auto",
+                trust_remote_code=True,
+        )
+        #model.config.use_cache = False
+        #model.gradient_checkpointing_enable()
+        model = prepare_model_for_kbit_training(model)
+        peft_config = LoraConfig(
+            lora_alpha=16,
+            lora_dropout=0.1,
+            r=128,
+            bias="none",
+            task_type="CAUSAL_LM",
+            # use_rslora=True,
+            # use_dora=True,
+        )
+        model = get_peft_model(model, peft_config)
+    else:
+        # with init_empty_weights():
+        #     model = AutoModelForCausalLM.from_config(model)
+
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model,
+            device_map="auto",
+            trust_remote_code=True,
+        )
+
+        # Загрузка контрольной точки и распределение между устройствами
+        model = load_checkpoint_and_dispatch(
+            model,
+            checkpoint=base_model,  # Укажите путь к контрольной точке
+            device_map="auto",  # Автоматическое распределение между GPU и CPU
+            offload_folder="offload",  # Папка для выгрузки данных на диск
+        )
+
+        # Включение gradient checkpointing
+        model.gradient_checkpointing_enable()
+        model.config.use_cache = False
+
 LR = 0.0002
-epochs = 3
+epochs = 30
 max_length = 3000
 expected_max_tokens = 1000
-training_arguments = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=epochs,
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=4,
-    optim="paged_adamw_32bit",
-    save_steps=150,
-    logging_steps=25,
-    learning_rate=LR,
-    weight_decay=0.001,
-    fp16=False,
-    bf16=True,
-    max_grad_norm=0.3,
-    max_steps=-1,
-    warmup_ratio=0.03,
-    group_by_length=True,
-    lr_scheduler_type="constant",
-    report_to="wandb",
-    save_total_limit=3,
-)
-trainer = advanced_SFTTrainer(
-        model=model,
-        train_dataset=dataset,
-        peft_config=peft_config,
-        max_seq_length=4048,
-        dataset_text_field="text",
-        tokenizer=tokenizer,
-        args=training_arguments,
-        packing=False
+
+if device == "cpu":
+    training_arguments = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=epochs,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=1,
+        save_steps=15,
+        logging_steps=3,
+        learning_rate=LR,
+        weight_decay=0.001,
+        fp16=False,
+        bf16=False,
+        max_grad_norm=0.3,
+        max_steps=-1,
+        warmup_ratio=0.03,
+        group_by_length=True,
+        lr_scheduler_type="constant",
+        report_to="wandb",
+        save_total_limit=3,
+        use_cpu=True
     )
+
+else:
+    training_arguments = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=epochs,
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=1,
+        optim="paged_adamw_32bit",
+        save_steps=150,
+        logging_steps=25,
+        learning_rate=LR,
+        weight_decay=0.001,
+        fp16=False,
+        bf16=True,
+        max_grad_norm=0.3,
+        max_steps=-1,
+        warmup_ratio=0.03,
+        group_by_length=True,
+        lr_scheduler_type="constant",
+        report_to="wandb",
+        save_total_limit=3,
+    )
+
+if custom_loss:
+    if device == "cpu":
+        trainer = advanced_SFTTrainer(
+            model=model,
+            train_dataset=dataset,
+            max_seq_length=1024,
+            dataset_text_field="text",
+            tokenizer=tokenizer,
+            args=training_arguments,
+            packing=False
+        )
+
+    else:
+        if lora:
+            print("lora + cuda training")
+            trainer = advanced_SFTTrainer(
+                    model=model,
+                    train_dataset=dataset,
+                    peft_config=peft_config,
+                    max_seq_length=1024,
+                    dataset_text_field="text",
+                    tokenizer=tokenizer,
+                    args=training_arguments,
+                    packing=False
+                )
+        else:
+            trainer = advanced_SFTTrainer(
+                model=model,
+                train_dataset=dataset,
+                max_seq_length=1024,
+                dataset_text_field="text",
+                tokenizer=tokenizer,
+                args=training_arguments,
+                packing=False
+            )
+else:
+    if device == "cpu":
+        trainer = SFTTrainer(
+            model=model,
+            train_dataset=dataset,
+            max_seq_length=1024,
+            dataset_text_field="text",
+            tokenizer=tokenizer,
+            args=training_arguments,
+            packing=False
+        )
+    else:
+        trainer = SFTTrainer(
+                model=model,
+                train_dataset=dataset,
+                peft_config=peft_config,
+                max_seq_length=1024,
+                dataset_text_field="text",
+                tokenizer=tokenizer,
+                args=training_arguments,
+                packing=False
+            )
 
 
 def merge_model(base_path, adapter_path, save_to):
@@ -302,14 +446,24 @@ def merge_model(base_path, adapter_path, save_to):
     model = PeftModel.from_pretrained(base_model, adapter_path)
     model = model.merge_and_unload()
 
-    model.save_pretrained(save_to, safe_serialization=True, max_shard_size='4GB')
+    model.save_pretrained(save_to, safe_serialization=True, max_shard_size='22GB')
     tokenizer.save_pretrained(save_to)
+
+
+merge_model(base_model, model_name, "models/Mistral-7B-finetuned")
+print("merged")
+time.sleep(1000)
 
 def handle_stop_signal(signal_file):
     global trainer
+    try:
+        os.remove(signal_file)  # Удаляем файл-сигнал
+    except Exception:
+        pass
     while True:
         if os.path.exists(signal_file):
             print("Получен сигнал остановки. Завершаем обучение...")
+            trainer.control.should_training_stop = True
             trainer.save_model()  # Сохраняем модель
             new_model = "new_model"
             trainer.model.save_pretrained(new_model)
@@ -320,7 +474,11 @@ def handle_stop_signal(signal_file):
             merge_model(base_path, adapter_path, save_to)
 
             model.config.use_cache = True
-            os.remove(signal_file)  # Удаляем файл-сигнал
+            try:
+                os.remove(signal_file)  # Удаляем файл-сигнал
+            except Exception:
+                pass
+            print("Saved, finish process")
             sys.exit(0)  # Завершаем процесс
         time.sleep(1)
 
@@ -333,3 +491,13 @@ stop_signal_thread = threading.Thread(target=handle_stop_signal, args=(signal_fi
 stop_signal_thread.start()
 
 trainer.train()
+
+trainer.save_model()  # Сохраняем модель
+new_model = "new_model"
+trainer.model.save_pretrained(new_model)
+base_path = base_model  # input: base model
+adapter_path = new_model  # input: adapters
+save_to = "models/Mistral-7B-finetuned2"  # out: merged model ready for inference
+
+merge_model(base_path, adapter_path, save_to)
+
